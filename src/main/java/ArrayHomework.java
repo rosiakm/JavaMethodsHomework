3735893c82550e07c3452662259f790015084f38
matros <mateusz.rosiak89@gmail.com>
2022-08-03 16:30:44 +0200

    Initial commit

---
+++ src/main/java/ArrayHomework.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayHomework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide an integer from 1 to 10: ");
+        int size = scanner.nextInt();
+
+        double[] array = new double[size];
+
+        for (int i = 0; i < size; i++){
+            System.out.println("Please provide an integer to store in the table for index: " + i);
+            double value = scanner.nextInt();
+            array[i] = value;
+        }
+
+        double sum = 0;
+        for (double i:array) {
+            sum += i;
+        }
+        double avgValue = sum/array.length;
+        double maxValue = Arrays.stream(array).max().getAsDouble();
+
+        System.out.println("And average value is: " + avgValue);
+        System.out.println("The maximum value is: " + maxValue);
+    }
+}
